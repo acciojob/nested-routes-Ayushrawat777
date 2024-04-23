cefce42ee211ccac6f7d7d8ea6992292c20cdd17
AYUSH <106034287+Ayushrawat777@users.noreply.github.com>
2024-04-23 09:30:58 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,42 @@
-
 import React from "react";
-import './../styles/App.css';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  NavLink,
+} from "react-router-dom";
+
+import Home from "./Home";
+import Women from "./Women";
+import Grooming from "./Grooming";
+import Shirt from "./Shirt";
+import Trouser from "./Trouser";
+import Jewellery from "./Jewellery";
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    <Router>
+      <h1>React Router</h1>
 
-export default App
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/women">User</NavLink>
+      </nav>
+
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/" element={<Home />} />
+
+        <Route path="/women" element={<Women />}>
+          <Route path="grooming" element={<Grooming />} />
+          <Route path="shirt" element={<Shirt />} />
+          <Route path="trouser" element={<Trouser />} />
+          <Route path="jewellery" element={<Jewellery />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
